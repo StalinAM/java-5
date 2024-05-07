9af51768ca64dfad79aadf3350af02024b26c4ec
Stalin <sta_vla411@hotmail.com>
2024-05-07 09:22:14 -0500

    add read all products

--- JavaPostgresql/src/Singleton/ProductCRUD.java
+++ JavaPostgresql/src/Singleton/ProductCRUD.java
@@ -2,7 +2,12 @@ package Singleton;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductCRUD {
 
@@ -10,8 +15,8 @@ public class ProductCRUD {
 
 	// CREAR PRODUCTO
 	public static void createProduct(Producto producto) {
-		//USO TRY WITH RESOURCES
-		//La conexion a la base de datos se cierra automaticamente al terminar
+		// USO TRY WITH RESOURCES
+		// La conexion a la base de datos se cierra automaticamente al terminar
 		try (Connection connection = ConexionBase.getConnection();
 				PreparedStatement statement = connection
 						.prepareStatement("INSERT INTO " + TABLE_NAME + " (nombre, cantidad) VALUES (?, ?)")) {
@@ -23,6 +28,23 @@ public class ProductCRUD {
 		}
 	}
 
-	
-	
+	public static List<Producto> readAllProducts() {
+		List<Producto> products = new ArrayList<>();
+		try (Connection connection = ConexionBase.getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM " + TABLE_NAME)) {
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String nombre = resultSet.getString("nombre");
+				int cantidad = resultSet.getInt("cantidad");
+
+				Producto product = new Producto(nombre, cantidad);
+				products.add(product);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return products;
+	}
+
 }
