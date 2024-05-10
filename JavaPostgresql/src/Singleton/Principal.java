fdc91773a65536c4461a961e57a8e8e3b84d1b91
Stalin <sta_vla411@hotmail.com>
2024-05-10 14:01:51 -0500

    view all products

--- JavaPostgresql/src/Singleton/Principal.java
+++ JavaPostgresql/src/Singleton/Principal.java
@@ -3,14 +3,19 @@ package Singleton;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Producto producto1 = new Producto("mesa", 2);
-		ProductCRUD.createProduct(producto1);
+		//Producto producto1 = new Producto("mesa", 2);
+		//ProductCRUD.createProduct(producto1);
+		List<Producto> allProducts = ProductCRUD.readAllProducts();
+		for (Producto product : allProducts) {
+		    System.out.println(product);
+		}
 	}
 
 }
