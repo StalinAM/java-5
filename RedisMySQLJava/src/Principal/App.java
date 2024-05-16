89237e47b728e1f0f4be51f7acd8c8a9d24b4a39
Stalin <sta_vla411@hotmail.com>
2024-05-15 21:45:23 -0500

    add connection mysql and redis

--- RedisMySQLJava/src/Principal/App.java
+++ RedisMySQLJava/src/Principal/App.java
@@ -1,25 +1,49 @@
 package Principal;
 
-import java.util.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import SQL.ConexionDB;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 
-public class CRUD {
+public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Jedis pool = new Jedis("localhost",6379);
-		
-		try {
-			System.out.println(pool.get("key"));
-		} catch (Exception e) {
-			// TODO: handle exception
-		} finally {
-			pool.close();
+		Jedis pool = new Jedis("localhost", 6379);
+		long startTime = System.currentTimeMillis();
+		if (pool.get("datos") != null) {
+			System.out.println("Existe esa clave!");
+		} else {
+			int count = 0;
+			StringBuilder datos = new StringBuilder();
+			try (Connection connection = ConexionDB.getConnection();
+					Statement statement = connection.createStatement();
+					ResultSet resultSet = statement
+							.executeQuery("Select e.first_name, e.last_name, s.salary, d.dept_name\r\n"
+									+ "from employees e, salaries s, dept_emp dep, departments d\r\n"
+									+ "where s.emp_no = e.emp_no and e.emp_no = dep.emp_no and dep.dept_no = d.dept_no;")) {
+				while (resultSet.next()) {
+					java.sql.ResultSetMetaData rsmd = resultSet.getMetaData();
+					count++;
+					for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+						datos.append(resultSet.getString(i));
+					}
+				}
+				pool.set("datos", datos.toString());
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				pool.close();
+			}
+			System.out.println(count);
+
 		}
-
-		
-
+		long endTime = System.currentTimeMillis();
+		long executionTime = endTime - startTime;
+		double executionTimeInSeconds = (double) executionTime / 1000;
+		System.out.println("Tiempo de ejecucion: " + executionTimeInSeconds + " seconds");
 	}
-
 }
--- RedisMySQLJava/src/SQL/ConexionDB.java
+++ RedisMySQLJava/src/SQL/ConexionDB.java
@@ -1,6 +1,27 @@
-package Conexion;
+package SQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ConexionDB {
-	
+	public static Connection getConnection() {
+
+		String url = "jdbc:mysql://localhost:3306/employees";
+		String usuario = "root";
+		String contrasenia = "root";
+
+		Connection connection = null;
+
+		if (connection == null) {
+			try {
+				connection = DriverManager.getConnection(url, usuario, contrasenia);
+				boolean valid = connection.isValid(5000);
+				System.out.println(valid ? "Connection realizada" : "Connection fallida");
+			} catch (java.sql.SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
 }
