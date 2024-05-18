bc092e17f233038b3d363cc6960e0cb6863e8896
Stalin <sta_vla411@hotmail.com>
2024-05-17 21:04:03 -0500

    add get and set

--- PatronFactory/src/modelos/PodructosRefrigerados.java
+++ PatronFactory/src/modelos/PodructosRefrigerados.java
@@ -3,5 +3,32 @@ package modelos;
 import modelos.abs.Productos;
 
 public class PodructosRefrigerados extends Productos {
+	private String codigoOSA;
+	private String fechaEnvasado;
+	private int temperaturaRecom;
+
+	public String getCodigoOSA() {
+		return codigoOSA;
+	}
+
+	public void setCodigoOSA(String codigoOSA) {
+		this.codigoOSA = codigoOSA;
+	}
+
+	public String getFechaEnvasado() {
+		return fechaEnvasado;
+	}
+
+	public void setFechaEnvasado(String fechaEnvasado) {
+		this.fechaEnvasado = fechaEnvasado;
+	}
+
+	public int getTemperaturaRecom() {
+		return temperaturaRecom;
+	}
+
+	public void setTemperaturaRecom(int temperaturaRecom) {
+		this.temperaturaRecom = temperaturaRecom;
+	}
 
 }
