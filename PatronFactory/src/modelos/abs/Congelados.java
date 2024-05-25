55d1b6f8f97e1f1bff067f145922b73d7400f511
Stalin <sta_vla411@hotmail.com>
2024-05-24 19:13:37 -0500

    add get and set

--- PatronFactory/src/modelos/abs/Congelados.java
+++ PatronFactory/src/modelos/abs/Congelados.java
@@ -1,8 +1,15 @@
 package modelos.abs;
 
 public abstract class Congelados extends Productos {
-	
+
 	private String temperatura;
 
+	public String getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(String temperatura) {
+		this.temperatura = temperatura;
+	}
 
 }
